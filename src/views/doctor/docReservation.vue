4d262337637152d1ed981d43f2ae2655158b564a
hejiakai <hejiakai@fu-rui.com>
2017-04-21 09:58:05 +0800

    医生完善信息页 预约管理列表

--- src/views/doctor/docReservation.vue
+++ src/views/doctor/docReservation.vue
@@ -2,38 +2,57 @@
 <template>
   <div class="doc-reservation-box">
     <div style="padding:10px 16px;">
-      <div class="doc-reservation-list-box">
+      <div v-for="item in items" class="doc-reservation-list-box">
         <div class="doc-reservation-list-content-top">
-        <div class="doc-reservation-list-content-top-left">
-        <span class="doc-reservation-list-content-top-span doc-reservation-list-content-top-name">张飞</span>
-        <span class="doc-reservation-list-content-top-span">男</span>
-        <span class="doc-reservation-list-content-top-span">28</span>
-        <span class="doc-reservation-list-content-top-span">丙肝</span>
+          <div class="doc-reservation-list-content-top-left">
+            <span class="doc-reservation-list-content-top-span doc-reservation-list-content-top-name">张飞</span>
+            <span class="doc-reservation-list-content-top-span">男</span>
+            <span class="doc-reservation-list-content-top-span">28</span>
+            <span class="doc-reservation-list-content-top-span">丙肝</span>
+          </div>
+          <div class="doc-reservation-list-content-top-right">
+            <span class="doc-reservation-list-content-top-span-status">待确认</span>
+          </div>
         </div>
-        <div class="doc-reservation-list-content-top-right">
-        <span class="doc-reservation-list-content-top-span-status">待确认</span>
+        <div class="doc-reservation-list-content-mid">
+          <div class="doc-reservation-list-content-mid-top">
+            <span>就诊时间:</span>
+            <span>2017-02-02</span>
+            <span>周二</span>
+            <span>上午</span>
+          </div>
+          <div class="doc-reservation-list-content-mid-line"></div>
         </div>
+        <div class="doc-reservation-list-content-bottom">
+          2017-02-01
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-
+  export default {
+    name : 'docReservation',
+    data () {
+      return {
+        items : [1,2,3,3,3,3,3,3,3]
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
   .doc-reservation-box {
     background-color: #f4f4f4;
-    height: 100vh;
+    min-height: 100vh;
 
     .doc-reservation-list-box {
       /*margin: 20px;*/
       /*padding-top: 20px;*/
       margin-top: 20px;
       background-color: #fff;
+      box-shadow:0px 0px 5px #7e8c8d; /* 边框阴影效果 */
       border-radius: 3px;
 
         .doc-reservation-list-content-top {
@@ -44,25 +63,50 @@
 
           .doc-reservation-list-content-top-left {
             color: #232323;
-            font-size: 14px;
+            font-size: 12px;
 
             .doc-reservation-list-content-top-span {
-              padding-left: 10px;
+              padding-left: 15px;
             }
 
             .doc-reservation-list-content-top-name {
-              font-size: 16px;
+              font-size: 14px;
             }
 
           }
           .doc-reservation-list-content-top-right {
-            font-size: 16px;
-            padding-right: 10px;
+            font-size: 14px;
+            padding-right: 15px;
 
             .doc-reservation-list-content-top-span-status {
               color: #ff0000;
             }
           }
+        }
+        .doc-reservation-list-content-mid {
+          padding-top: 15px;
+
+          .doc-reservation-list-content-mid-top {
+            padding-left: 15px;
+            font-size: 12px;
+          }
+
+          .doc-reservation-list-content-mid-line {
+            height: 1px;
+            background: #eeeeee;
+            margin-top: 10px;
+            margin-left: 10px;
+            margin-right: 10px;
+          }
+        }
+
+        .doc-reservation-list-content-bottom {
+          padding-top: 10px;
+          padding-bottom: 12px;
+          text-align: right;
+          padding-right: 15px;
+          font-size: 12px;
+          color: #D2D2D2;
         }
     }
   }
--- src/views/doctor/doctorPerfectInfo.vue
+++ src/views/doctor/doctorPerfectInfo.vue
@@ -1,44 +1,35 @@
 <!-- 完善信息 -->
 <template>
   <div class="perfectInfoContainer">
-    <div class="perfectInfoNameBox">
-      <span>姓名</span>
-      <input class="perfectInfoInput" placeholder="请输入姓名" maxlength="8">
-    </div>
-    <div class="perfectInfoLine">
-      <div class="perfectInfoLineThis"></div>
-    </div>
-    <div class="perfectInfoDepartmentBox">
-      <span>科室</span>
-      <select class="perfectInfoSelect">
-        <option>主治医师</option>
-        <option>主任</option>
-        <option>副主任</option>
-      </select>
-      <span class="perfectInfoIcon iconfont  icon-jiantou"></span>
-    </div>
-    <div class="perfectInfoLine">
-      <div class="perfectInfoLineThis"></div>
-    </div>
-    <div class="perfectInfoDepartmentBox">
-      <span>职称</span>
-      <select class="perfectInfoSelect">
-        <option>主治医师</option>
-        <option>主任</option>
-        <option>副主任</option>
-      </select>
-      <span class="perfectInfoIcon iconfont  icon-jiantou"></span>
-    </div>
-    <div class="perfectInfoLine">
-      <div class="perfectInfoLineThis"></div>
-    </div>
-    <div>
-      <button class="perfectInfoButton">
-        <label class="mint-button-text">确认</label>
-      </button>
-    </div>
-  </div>
+    <div class="perfectInfoTopBox">
+      <div class="perfectInfoTopName">
+        <span class="perfectInfoTopNameSpan">姓名</span>
+        <input class="perfectInfoTopNameInput" placeholder="请输入姓名" maxlength="6">
+      </div>
 
+      <div class="perfectInfoTopDepartment">
+        <span class="perfectInfoTopDepartmentSpan">科室</span>
+        <select class="perfectInfoTopDepartmentSelect">
+          <option value="0">请选择科室</option>
+          <option>肝病科</option>
+          <option>肝病科23室</option>
+          <option>精神科78室</option>
+        </select>
+      </div>
+
+      <div class="perfectInfoTopTitle">
+        <span class="perfectInfoTopTitleSpan">职称</span>
+        <select class="perfectInfoTopTitleSelect">
+          <option value="0">请选择职称</option>
+          <option>主治医师</option>
+          <option>主任</option>
+          <option>副主任</option>
+        </select>
+      </div>
+    </div>
+
+    <button class="perfectInfoBottomButton">确 认</button>
+  </div>
 </template>
 
 <script>
@@ -48,66 +39,95 @@
 <style lang="scss">
 
   .perfectInfoContainer {
+    position: fixed; /* 固定 */
     height: 100vh;
     width: 100%;
     background-image: url('../../assets/img/bgPerfectInfo.png');
     background-size: 100% 100%;
 
-    .perfectInfoNameBox {
-      line-height: 50px;
-      color: #ffffff;
-      padding-top: 50px;
-      padding-left: 50px;
-      font-size: 15px;
+    .perfectInfoTopBox {
+      margin: 50px auto;
+      width: 70%;
 
-      .perfectInfoInput {
-        padding-left: 30px;
-        text-align: center;
-        /*filter:alpha(opacity:30); opacity:0.3;  -moz-opacity:0.3;-khtml-opacity: 0.3*/
-        background-color:transparent;
-        color:#fff;
-        FILTER: alpha(opacity=0); /*androd*/
-        appearance:none;  /*下拉框去掉右侧图标*/
-        -moz-appearance:none;
-        -webkit-appearance:none;
+      .perfectInfoTopName {
+
+        .perfectInfoTopNameSpan {
+          color: #ffffff;
+        }
+        .perfectInfoTopNameInput {
+          background-color:transparent;
+          color:#fff;
+          FILTER: alpha(opacity=0); /*androd*/
+          appearance:none;  /*下拉框去掉右侧图标*/
+          -moz-appearance:none;
+          -webkit-appearance:none;
+          float: right;
+          outline: none;
+          text-align: center;
+          width: 80%;
+          border-bottom: 1px solid #fff;
+          padding-bottom: 10px;
+        }
+      }
+
+      .perfectInfoTopDepartment {
+        padding-top: 70px;
+        .perfectInfoTopDepartmentSpan {
+          color: #ffffff;
+        }
+        .perfectInfoTopDepartmentSelect {
+          float: right;
+          color: #ffffff;
+          width: 80%;
+          border: none;
+          border-bottom: 1px solid #ffffff;
+          padding-bottom: 10px;
+          padding-left: 25%;
+          outline: none;
+          /*background: rgba(255,255,255,0);*/
+          background-color:transparent;
+          -webkit-appearance:none;
+          -webkit-border-radius: 0px;
+          font-size: 15px;
+        }
+      }
+
+      .perfectInfoTopTitle {
+        padding-top: 70px;
+        .perfectInfoTopTitleSpan {
+          color: #ffffff;
+        }
+        .perfectInfoTopTitleSelect {
+          float: right;
+          color: #ffffff;
+          width: 80%;
+          border: none;
+          border-bottom: 1px solid #ffffff;
+          padding-bottom: 10px;
+          padding-left: 25%;
+          outline: none;
+          /*background: rgba(255,255,255,0);*/
+          background-color:transparent;
+          -webkit-appearance:none;
+          -webkit-border-radius: 0px;
+          font-size: 15px;
+        }
       }
     }
 
-  /* 下划线 */
-  .perfectInfoLine {
-    width:100%;
-    .perfectInfoLineThis {
-      height:1px;
-      width:50%;
-      margin:0 35% 0;
-      background:#ffffff;
-    }
-  }
-
-  .perfectInfoDepartmentBox {
-    line-height: 50px;
-    color: #ffffff;
-    padding-top: 30px;
-    padding-left: 50px;
-
-    .perfectInfoSelect {
+    .perfectInfoBottomButton {
+      display: block;
+      width: 90%;
+      height:45px;
+      margin: auto;
+      /*background:#529D98;*/
+      background: rgba(0,0,0,0);
       color: #ffffff;
-      margin-left: 85px;
+      border:1px solid #86B8B8;
+      border-radius:22px;
+      margin-top:160px;
+      outline: none;
+      font-size: 16px;
     }
-    .perfectInfoIcon {
-      font-size: 18px;
-    }
-  }
-  .perfectInfoButton {
-    display: block;
-    width: 90%;
-    height:45px;
-    margin: auto;
-    background:#529D98;
-    color: #ffffff;
-    border:1px solid #86B8B8;
-    border-radius:22px;
-    margin-top:160px;
-  }
   }
 </style>
