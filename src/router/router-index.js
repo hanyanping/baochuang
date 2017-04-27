0c96e263fdec454e0a6ef8dfd6a615ede568558e
lihao <nicolalh@163.com>
2017-04-27 20:15:02 +0800

    更换主管医生

--- src/router/router-index.js
+++ src/router/router-index.js
@@ -31,6 +31,7 @@ import doctorInfo from '../views/patient/doctorInfo'
 import selectTestReportCheck from '../views/patient/selectTestReportCheck'
 import checkRecord from '../views/patient/checkRecord'
 import clinicRecord from '../views/patient/clinicRecord'
+import changeMainDoctor from '../views/patient/changeMainDoctor'
 
 
 
@@ -90,6 +91,7 @@ export default new Router({
         {path: 'selectTestReportCheck',component: selectTestReportCheck},
         {path: 'checkRecord',component: checkRecord},
         {path: 'clinicRecord',component: clinicRecord},
+        {path: 'changeMainDoctor',component: changeMainDoctor},
       ],
     },
     {
