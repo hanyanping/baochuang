657792f944f164d2032aac524854ea361e8a0621
gefangyan <gefangyan@fu-rui.com>
2017-04-28 17:05:29 +0800

    发布代码

--- src/main.js
+++ src/main.js
@@ -21,8 +21,7 @@ rootRouter.beforeEach(function (to, from, next) {
   // 从localStorage中获取auth
   var auth = localStorage.getItem('auth');
   // var auth = 'fff'; // 测试使用
-  localStorage.clear();
-  auth = '';
+  // localStorage.clear();
   if (auth == null || auth == '' || auth == undefined) {
     // 从URL中获取auth , 对URL进行解析
     auth = getParameterByName('authentication');
