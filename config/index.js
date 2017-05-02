a48b3f74032693e1f21c5f468de9b6a38b9fcc75
gefangyan <gefangyan@fu-rui.com>
2017-05-02 11:18:21 +0800

    发布代码

--- config/index.js
+++ config/index.js
@@ -30,7 +30,8 @@ module.exports = {
     proxyTable: {
       '/api': {
         // target: 'http://lipengwei.furui.com',      // 个人服务器
-        target:  'http://testbdwx.aiganyisheng.com/', // 测试服务器
+        // target:  'http://testbdwx.aiganyisheng.com', // 测试服务器
+        target:  'http://testaiganneo.aiganyisheng.com',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
